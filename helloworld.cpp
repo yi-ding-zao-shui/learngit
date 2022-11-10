cd5c2d19d9ebf38957f8c98b95d16a28b3366afc
tanjiahao <2847166564@qq.com>
2022-11-10 08:38:29 +0800

    改为输出多行hello,world!

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main() {
     cout << "hello, world!" << endl;
+    cout << "hello, world!" << endl;
+    cout << "hello, world!" << endl;
+    cout << "hello, world!" << endl;
+
     return 0;
 }
