194f7bc42cfc88205e7e5a83ca684aadf23c4306
tanjiahao <2847166564@qq.com>
2022-11-12 15:15:38 +0800

    实现加法

---
+++ 计算器.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int add(int x, int y) {
+    return x + y;
+}
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << add(x, y) << endl;
+    return 0;
+}
